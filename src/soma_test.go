b033989efe4a9f7015bc502fe555088009f5dbc4
Robson Inocêncio <robson.inocencio@gmail.com>
2020-08-31 05:07:05 -0300

    first commit

---
+++ src/soma_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T){
+  resultado := Soma(5,5)
+  if resultado != 10{
+    t.Errorf("Resultado esperado: %d, Resultado obtido: %d", 10, resultado)
+  }
+}
